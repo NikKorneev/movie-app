c74c5aa73f0e5876e5625b4c8339cc61b17dd8b6
Nick <Nikita_korneev101@mail.ru>
2024-07-10 21:58:15 +0300

    initial setup

---
+++ app/_layout.tsx
@@ -0,0 +1,36 @@
+import "@/global.css";
+
+import { useFonts } from "expo-font";
+import { Stack } from "expo-router";
+import * as SplashScreen from "expo-splash-screen";
+import { useEffect } from "react";
+import "react-native-reanimated";
+
+import { SafeAreaView } from "react-native-safe-area-context";
+
+// Prevent the splash screen from auto-hiding before asset loading is complete.
+SplashScreen.preventAutoHideAsync();
+
+export default function RootLayout() {
+	const [loaded] = useFonts({
+		SpaceMono: require("../assets/fonts/SpaceMono-Regular.ttf"),
+	});
+
+	useEffect(() => {
+		if (loaded) {
+			SplashScreen.hideAsync();
+		}
+	}, [loaded]);
+
+	if (!loaded) {
+		return null;
+	}
+
+	return (
+		<SafeAreaView>
+			<Stack>
+				<Stack.Screen name="index" />
+			</Stack>
+		</SafeAreaView>
+	);
+}
